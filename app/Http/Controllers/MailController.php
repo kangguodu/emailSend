5ae88a4c4fae59dc17c05ce2c4662c7cc30877d5
kgd <2595389179@qq.com>
2018-06-07 17:05:00 +0800

    邮箱发送

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Mail;
+
+class MailController extends Controller
+{
+    public function send()
+    {
+        $name = '学院君';
+        $flag = Mail::send('email.test',['name'=>'kgd'],function($message){
+            $to = '13612833901@163.com';
+            $message ->to($to)->subject('测试邮件');
+        });
+        if($flag){
+            echo '发送邮件成功，请查收！';
+        }else{
+            echo '发送邮件失败，请重试！';
+        }
+    }
+}
+
